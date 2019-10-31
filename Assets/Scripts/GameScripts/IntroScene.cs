df9e9d595033b688724458a74e6f0e210cade37a
Krupal Joshi <krupal.joshi@gmail.com>
2019-10-31 17:37:02 +0900

    Added intro scene and logo

---
+++ Assets/Scripts/GameScripts/IntroScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadMainMenu());
+    }
+
+    IEnumerator LoadMainMenu()
+    {
+
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene("MenuScene");
+
+    }
+}
